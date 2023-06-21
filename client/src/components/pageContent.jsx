9dc0f577caaeb1954ffd9bd75a02aae25b7119f8
lidlessey3 <leonardo.pavarino@gmail.com>
2023-06-21 19:09:16 +0200

    editor inner workings done

--- client/src/components/pageContent.jsx
+++ client/src/components/pageContent.jsx
@@ -13,11 +13,16 @@ function ContentHeather(props) {
 function ContentHeatherEditor(props) {
     return (
         <>
-            <div className='contentEditor'>
-                <Button variant='danger-outline' onClick={(event)=>props.remove()}><i className='bi bi-x-lg'></i></Button>
-                <Form>
+            <div className='contentEditor card'>
+                <div className='d-flex justify-content-start align-items-center'>
+                    <Button variant='outline-danger' onClick={(event) => props.remove()} className='margin-right-02rem'><i className='bi bi-x-lg'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveUp()} className='margin-right-02rem'><i className='bi bi-arrow-up'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveDown()} className='margin-right-02rem'><i className='bi bi-arrow-down'></i></Button>
+                    <h4>Header</h4>
+                </div>
+                <Form className='margin-top-05rem'>
                     <Form.Control type='text' value={props.data.elementData} onChange={(event) => {
-                        let newElement;
+                        let newElement = {};
                         newElement.elementType = props.data.elementType;
                         newElement.elementData = event.target.value;
                         props.updateItem(newElement);
@@ -35,32 +40,42 @@ function ContentText(props) {
 function ContentTextEditor(props) {
     return (
         <>
-            <div className='contentEditor'>
-                <Button variant='danger-outline' onClick={(event)=>props.remove()}><i className='bi bi-x-lg'></i></Button>
-                    <textarea value={props.data.elementData} onChange={(event) => {
-                        let newElement;
-                        newElement.elementType = props.data.elementType;
-                        newElement.elementData = event.target.value;
-                        props.updateItem(newElement);
-                    }}></textarea>
+            <div className='contentEditor card'>
+                <div className='d-flex justify-content-start align-items-center'>
+                    <Button variant='outline-danger' onClick={(event) => props.remove()} className='margin-right-02rem'><i className='bi bi-x-lg'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveUp()} className='margin-right-02rem'><i className='bi bi-arrow-up'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveDown()} className='margin-right-02rem'><i className='bi bi-arrow-down'></i></Button>
+                    <h4>Text</h4>
+                </div>
+                <textarea value={props.data.elementData} onChange={(event) => {
+                    let newElement = {};
+                    newElement.elementType = props.data.elementType;
+                    newElement.elementData = event.target.value;
+                    props.updateItem(newElement);
+                }} className='margin-top-05rem bg-light text-dark'></textarea>
             </div>
         </>
     );
 }
 
 function ContentImage(props) {
-    
+
 }
 
 function ContentImageEditor(props) {
     return (
         <>
-            <div className='contentEditor'>
-                <Button variant='danger-outline' onClick={(event) => props.remove()}><i className='bi bi-x-lg'></i></Button>
-                <img src={images[props.data.elementData]}></img>
-                <Form>
+            <div className='contentEditor card'>
+                <div className='d-flex justify-content-start align-items-center'>
+                    <Button variant='outline-danger' onClick={(event) => props.remove()} className='margin-right-02rem'><i className='bi bi-x-lg'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveUp()} className='margin-right-02rem'><i className='bi bi-arrow-up'></i></Button>
+                    <Button variant='outline-info' onClick={(event) => props.moveDown()} className='margin-right-02rem'><i className='bi bi-arrow-down'></i></Button>
+                    <h4>Image</h4>
+                </div>
+                <img src={images[props.data.elementData]} className='content-image margin-top-05rem'></img>
+                <Form className='margin-top-05rem'>
                     <Form.Select value={props.data.elementData} onChange={(event) => {
-                        let newElement;
+                        let newElement = {};
                         newElement.elementType = props.data.elementType;
                         newElement.elementData = event.target.value;
                         props.updateItem(newElement);
--- client/src/components/pageEditorForm.jsx
+++ client/src/components/pageEditorForm.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
-import { Row, Col, Form, Container } from 'react-bootstrap';
+import { Row, Col, Form, Container, Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import './../style/pageEditorForm.css'
 import dayjs from 'dayjs';
+import { ContentHeatherEditor, ContentImageEditor, ContentTextEditor } from "./pageContent";
 
 
 function PageEditorForm(props) {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState(undefined);
     const [components, setComponents] = useState([]);
-    const [publicationDate, setPublicationDate] = useState(dayjs());
+    const [publicationDate, setPublicationDate] = useState(undefined);
     const [users, setUsers] = useState([]);
     const navigate = useNavigate();
 
@@ -52,20 +53,24 @@ function PageEditorForm(props) {
                     </Col>
                     <Col md="auto" id="pageSettings">
                         <Form>
-                            <div className="d-flex justify-content-beginning align-items-center">
+                            <div className="d-flex justify-content-beginning align-items-center margin-top-05rem">
                                 Author: {props.user.admin === 1 ? <Form.Select className="w-auto" value={JSON.stringify(author)} onChange={(event) => {
                                     setAuthor(JSON.parse(event.target.value));
                                 }}>
                                     {users.map((user) => <option key={user.id} value={JSON.stringify(user)}>{user.name}</option>)}
                                 </Form.Select> : props.user.name}
                             </div>
-                            <div className="d-flex justify-content-beginning align-items-center">
-                                Publication date: <Form.Control type='date' className="w-auto" value={publicationDate.format('YYYY-MM-DD')} onChange={(event) => {
-                                    setPublicationDate(dayjs(event.target.value, 'YYYY-MM-DD'))
-                                }}></Form.Control>
+                            <div className="d-flex justify-content-beginning align-items-center margin-top-05rem">
+                                Publication date: <Form.Control type='date' className="w-auto" value={publicationDate ? publicationDate.format('YYYY-MM-DD') : ''} onChange={
+                                    (event) => {
+                                        if (event.target.value !== '')
+                                            setPublicationDate(dayjs(event.target.value, 'YYYY-MM-DD'));
+                                        else
+                                            setPublicationDate(undefined);
+                                    }}></Form.Control>
                             </div>
                         </Form>
-                        <PageComponent></PageComponent>
+                        <PageComponentAdder components={components} setComponents={setComponents}></PageComponentAdder>
                     </Col>
                 </> : <></>}
             </Row>
@@ -73,21 +78,67 @@ function PageEditorForm(props) {
     );
 }
 
-function PageComponent(props) {
+function PageComponentAdder(props) {
     return (
         <>
-            
+            <Button variant="outline-primary" className="w-100 margin-top-05rem" onClick={(event) => {
+                props.setComponents([...props.components, { elementType: 'header', elementData: '' }]);
+            }}>Add Header</Button>
+            <Button variant="outline-primary" className="w-100 margin-top-05rem" onClick={(event) => {
+                props.setComponents([...props.components, { elementType: 'text', elementData: '' }]);
+            }}>Add Text</Button>
+            <Button variant="outline-primary" className="w-100 margin-top-05rem" onClick={(event) => {
+                props.setComponents([...props.components, { elementType: 'image', elementData: 0 }]);
+            }}>Add Image</Button>
         </>
     );
 }
 
 function PageEditorArea(props) {
     return (<>
-        <Row className="d-flex w-100" id="dropArea">
-            <div className="w-100" id="backgroundText">
-                <p>Drag Something Here</p>
-                
-            </div>
+        <Row className="d-flex w-100 align-items-start flex-column" id="dropArea">
+            {props.components.length === 0 ? <div className="w-100" id="backgroundText">
+                <p>Add something to start working</p>
+            </div> : <></>}
+            {props.components.map((element, index) => {
+                let remove = () => {
+                    let newComponents = Array(...props.components);
+                    newComponents.splice(index, 1);
+                    props.updateComponents(newComponents);
+                }
+                let updateItem = (newItem) => {
+                    let newComponents = Array(...props.components);
+                    newComponents.splice(index, 1, newItem);
+                    props.updateComponents(newComponents);
+                }
+                let moveUp = () => {
+                    if (index === 0)
+                        return;
+                    let newComponents = Array(...props.components);
+                    let [newItem] = newComponents.splice(index, 1);
+                    newComponents.splice(index - 1, 0, newItem);
+                    props.updateComponents(newComponents);
+                }
+                let moveDown = () => {
+                    if (index === props.components.length - 1)
+                        return;
+                    let newComponents = Array(...props.components);
+                    let [newItem] = newComponents.splice(index, 1);
+                    newComponents.splice(index + 1, 0, newItem);
+                    props.updateComponents(newComponents);
+                }
+                switch (element.elementType) {
+                    case 'header':
+                        return (<ContentHeatherEditor key={index} data={element} remove={remove} updateItem={updateItem}
+                            moveUp={moveUp} moveDown={moveDown}></ContentHeatherEditor>);
+                    case 'text':
+                        return (<ContentTextEditor key={index} data={element} remove={remove} updateItem={updateItem}
+                            moveUp={moveUp} moveDown={moveDown}></ContentTextEditor>);
+                    case 'image':
+                        return (<ContentImageEditor key={index} data={element} remove={remove} updateItem={updateItem}
+                            moveUp={moveUp} moveDown={moveDown}></ContentImageEditor>);
+                }
+            })}
         </Row>
     </>)
 }
