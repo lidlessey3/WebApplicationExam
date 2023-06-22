2a46959b04e064d8bdd5ace9e9df46dbfdc292ae
lidlessey3 <leonardo.pavarino@gmail.com>
2023-06-22 09:57:10 +0200

    completed error checking

--- client/src/components/pageEditorForm.jsx
+++ client/src/components/pageEditorForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Row, Col, Form, Container, Button } from 'react-bootstrap';
+import { Row, Col, Form, Container, Button, Collapse } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import './../style/pageEditorForm.css'
 import dayjs from 'dayjs';
@@ -12,6 +12,7 @@ function PageEditorForm(props) {
     const [components, setComponents] = useState([]);
     const [publicationDate, setPublicationDate] = useState(undefined);
     const [users, setUsers] = useState([]);
+    const [errors, setErrors] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -43,7 +44,8 @@ function PageEditorForm(props) {
                                 <div className="w-100">
                                     <Form>
                                         <div className="d-flex justify-content-center align-items-center" id="title-tile">
-                                            Title:<Form.Control type="text" required placeholder="Lorem Ipsum" value={title} onChange={(event) => setTitle(event.target.value)}></Form.Control><br />
+                                            Title:<Form.Control type="text" required placeholder="Lorem Ipsum" value={title}
+                                                onChange={(event) => setTitle(event.target.value)}></Form.Control><br />
                                         </div>
                                     </Form>
                                 </div>
@@ -51,7 +53,7 @@ function PageEditorForm(props) {
                             <PageEditorArea components={components} updateComponents={setComponents}></PageEditorArea>
                         </Container>
                     </Col>
-                    <Col md="auto" id="pageSettings">
+                    <Col md={2} id="pageSettings">
                         <Form>
                             <div className="d-flex justify-content-beginning align-items-center margin-top-05rem">
                                 Author: {props.user.admin === 1 ? <Form.Select className="w-auto" value={JSON.stringify(author)} onChange={(event) => {
@@ -61,16 +63,19 @@ function PageEditorForm(props) {
                                 </Form.Select> : props.user.name}
                             </div>
                             <div className="d-flex justify-content-beginning align-items-center margin-top-05rem">
-                                Publication date: <Form.Control type='date' className="w-auto" value={publicationDate ? publicationDate.format('YYYY-MM-DD') : ''} onChange={
-                                    (event) => {
-                                        if (event.target.value !== '')
-                                            setPublicationDate(dayjs(event.target.value, 'YYYY-MM-DD'));
-                                        else
-                                            setPublicationDate(undefined);
-                                    }}></Form.Control>
+                                Publication date: <Form.Control type='date' className="w-auto"
+                                    value={publicationDate ? publicationDate.format('YYYY-MM-DD') : ''} onChange={
+                                        (event) => {
+                                            if (event.target.value !== '')
+                                                setPublicationDate(dayjs(event.target.value, 'YYYY-MM-DD'));
+                                            else
+                                                setPublicationDate(undefined);
+                                        }}></Form.Control>
                             </div>
                         </Form>
                         <PageComponentAdder components={components} setComponents={setComponents}></PageComponentAdder>
+                        <PageSave components={components} title={title} publicationDate={publicationDate} author={author} setErrors={setErrors}></PageSave>
+                        <SaveError errors={errors}></SaveError>
                     </Col>
                 </> : <></>}
             </Row>
@@ -105,12 +110,12 @@ function PageEditorArea(props) {
                     let newComponents = Array(...props.components);
                     newComponents.splice(index, 1);
                     props.updateComponents(newComponents);
-                }
+                };
                 let updateItem = (newItem) => {
                     let newComponents = Array(...props.components);
                     newComponents.splice(index, 1, newItem);
                     props.updateComponents(newComponents);
-                }
+                };
                 let moveUp = () => {
                     if (index === 0)
                         return;
@@ -118,7 +123,7 @@ function PageEditorArea(props) {
                     let [newItem] = newComponents.splice(index, 1);
                     newComponents.splice(index - 1, 0, newItem);
                     props.updateComponents(newComponents);
-                }
+                };
                 let moveDown = () => {
                     if (index === props.components.length - 1)
                         return;
@@ -126,7 +131,7 @@ function PageEditorArea(props) {
                     let [newItem] = newComponents.splice(index, 1);
                     newComponents.splice(index + 1, 0, newItem);
                     props.updateComponents(newComponents);
-                }
+                };
                 switch (element.elementType) {
                     case 'header':
                         return (<ContentHeatherEditor key={index} data={element} remove={remove} updateItem={updateItem}
@@ -141,6 +146,47 @@ function PageEditorArea(props) {
             })}
         </Row>
     </>)
+}
+
+function PageSave(props) {
+    return (<>
+        <Button variant="outline-success" onClick={() => {
+            let actualComponents = Array(...props.components).filter((component) => component.elementData !== '');
+            let errors = [];
+            if (props.title === '')
+                errors.push('The article must have a title.');
+            let headerComponentsNum = actualComponents.filter((component) => component.elementType === 'header').length;
+            if (headerComponentsNum === 0)
+                errors.push('The article must have at least an header component.');
+            if ((actualComponents.length - headerComponentsNum) === 0)
+                errors.push('The article must have at least one among image or text components.');
+            if (props.publicationDate && props.publicationDate.isBefore(dayjs(), 'day'))
+                errors.push('The Publication date cannot be in the past.');
+            
+            if (errors.length === 0)
+                fetch('http://localhost:4452/api/pages/new', {
+                    credentials: 'include',
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ title: props.title, author: props.author.id, publicationDate: props.publicationDate.toISOString(), content: actualComponents })
+                });
+            else
+                props.setErrors(errors);
+        }} className="w-100 margin-top-05rem"><i className="bi bi-check2-circle">Save</i></Button>
+    </>);
+}
+
+function SaveError(props) {
+    console.log(props.errors);
+    return (
+        <>
+            <Collapse in={props.errors.length !== 0}>
+                <div className="card bg-danger text-light margin-top-05rem" id="errorContainer">
+                    {props.errors.map((err, index) => <p key={index}>{err}</p>)}
+                </div>
+            </Collapse>
+        </>
+    );
 }
 
 export default PageEditorForm;
