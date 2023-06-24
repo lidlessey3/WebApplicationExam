25583c0e2cda5b9f2eed6c05cef428f93666d12b
lidlessey3 <leonardo.pavarino@gmail.com>
2023-06-24 18:29:54 +0200

    basic 404

--- client/src/components/404.jsx
+++ client/src/components/404.jsx
@@ -1,5 +1,16 @@
-function NotFoundPage(props) {
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
+function NotFoundPage(props) {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="card">
+                <h1>Error 404</h1>
+                <Button onClick={() => navigate('/')}>Go back Home</Button>
+            </div>
+    </>
+    );
 }
 
 export default NotFoundPage;
