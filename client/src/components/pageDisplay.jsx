8a00cbe9395ba8f1f3bbed5c28ffc4a04297e80b
lidlessey3 <leonardo.pavarino@gmail.com>
2023-06-25 17:08:05 +0200

    added publication and creation date to page visualizer

--- client/src/components/pageDisplay.jsx
+++ client/src/components/pageDisplay.jsx
@@ -28,32 +28,35 @@ function PageDisplay(props) {
                     <i className="bi bi-arrow-left-circle" id="backArrow" onClick={() => {
                         navigate(-1);
                     }}></i>
-                    { props.user && page ? (props.user.admin || props.user.id === page.author.id) ? <>
+                    {props.user && page ? (props.user.admin || props.user.id === page.author.id) ? <>
                         <div className="outline-info card text-light margin-top-05rem">
                             <Link to='edit'><i className="bi bi-pencil-fill"></i></Link>
                         </div>
                         <Button variant='outline-danger' className="margin-top-05rem" onClick={() => {
                             deletePage(id).then((result) => {
-                                    if (!result.error) {
-                                        let newPages = [];
-                                        for (let i = 0; i < props.pages.length; i++) {
-                                            if (page.id !== props.pages[i].id)
-                                                newPages.push(props.pages[i]);
-                                        }
-                                        props.setPages(newPages);
-                                        navigate(-1);
+                                if (!result.error) {
+                                    let newPages = [];
+                                    for (let i = 0; i < props.pages.length; i++) {
+                                        if (page.id !== props.pages[i].id)
+                                            newPages.push(props.pages[i]);
                                     }
-                                    else
-                                        console.log(result.error);
-                                });
+                                    props.setPages(newPages);
+                                    navigate(-1);
+                                }
+                                else
+                                    console.log(result.error);
+                            });
                         }}><i className="bi bi-trash3-fill"></i></Button>
                     </> : <></> : <></>}
                 </Col>
                 <Col md={11} id="ViewContent">
                     <Container fluid className="d-flex flex-wrap">
                         <Row className="w-100">
-                            <div className="w-100 d-flex justify-content-center align-items-center">
-                                <h1>{page ? page.title : ''}</h1>
+                            <div className="w-100 d-flex justify-content-center align-items-center flex-column">
+                                <h1>{page ? page.title : ''}</h1><br />
+                                <h2>By {page.author.name}</h2><br />
+                                <h5>{page.publicationDate ? "Published on " + page.publicationDate.format('YYYY-MM-DD') : ''}
+                                    {page.creationDate ? ' Created on ' + page.creationDate.format('YYYY-MM-DD') : ''}</h5>
                             </div>
                         </Row>
                         <PageVisualize components={components}></PageVisualize>
